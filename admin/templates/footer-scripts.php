24e851fff0e6b5874b365ce41b984fe67429ac84
Carlosworld <49792178+Carlosworld@users.noreply.github.com>
2020-12-15 11:49:37 -0600

    Se esta integrando el diseño de la barra de navegacion del adminstrador

--- admin/templates/footer-scripts.php
+++ admin/templates/footer-scripts.php
@@ -1,33 +1,42 @@
+<!-- Sweetalert2 -->
+<script src="js/sweetalert2.all.min.js"></script>
 <!-- jQuery -->
 <script src="assets/js/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<!-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<!-- Select2 -->
+<script src="js/plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="js/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
+<script src="js/plugins/moment/moment.min.js"></script>
+<script src="js/plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+<!-- date-range-picker -->
+<script src="js/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="js/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="assets/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="assets/js/bootstrap-switch.min.js"></script>
 <!-- AdminLTE App -->
 <script src="assets/js/adminlte.min.js"></script>
-
-<script src="assets/js/adminlteDos.min.js"></script>
-
-<script src="js/sweetalert2.all.min.js"></script>
-
-<script src="assets/js/admin-ajax.js"></script>
-
-<!-- <script src="js/dataTables.bootstrap4.min.js"></script> -->
-
-<script src="js/jquery.dataTables.min.js"></script>
-
-<script src="assets/js/dataTables.responsive.min.js"></script>
-
-<script src="assets/js/responsive.bootstrap4.min.js"></script>
-
-<script src="js/buttons.bootstrap4.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="js/dist/js/demo.js"></script>
+<!-- DataTables -->
+<script src="js/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="js/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="js/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="js/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 
 <!-- AdminLTE for demo purposes -->
 <script src="assets/js/demo.js"></script>
 
-<script src="assets/js/app.js"></script>
+<script src="js/app.js"></script>
 
 <script src="js/login-admin.js"></script>
 
+<script src="js/admin-ajax.js"></script>
 
 
 </body>
--- admin/templates/header.php
+++ admin/templates/header.php
@@ -1,24 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>gdlwebcamp | administrador</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/css/all.min.css">
-  <!-- Ionicons -->
-  <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
-  <!-- overlayScrollbars -->
-  <!-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> -->
-  <script src="assets/css/buttons.bootstrap4.min.css"></script> 
-  <link rel="stylesheet" href="assets/css/adminlte.min.css">
-  <link rel="stylesheet" href="css/sweetalert2.min.css">
-  <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="assets/css/gdlwebcamp.css">
-  <link rel="stylesheet" href="assets/css/admin.css">
-  <link rel="stylesheet" href="assets/css/responsive.bootstrap4.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- admin -->
+<link rel="stylesheet" href="assets/css/admin.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="assets/css/all.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- daterange picker -->
+<link rel="stylesheet" href="js/plugins/daterangepicker/daterangepicker.css">
+<!-- iCheck for checkboxes and radio inputs -->
+<link rel="stylesheet" href="js/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+<!-- Bootstrap Color Picker -->
+<link rel="stylesheet" href="js/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
+<!-- Tempusdominus Bbootstrap 4 -->
+<link rel="stylesheet" href="assets/css/tem<pusdominus-bootstrap-4.min.css">
+<!-- Select2 -->
+<link rel="stylesheet" href="js/plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="js/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<!-- Bootstrap4 Duallistbox -->
+<link rel="stylesheet" href="js/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+<!-- Theme style -->
+<!-- DataTables -->
+<link rel="stylesheet" href="js/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="js/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="assets/css/adminlte.min.css">
+<!-- Google Font: Source Sans Pro -->
+<!-- Sweetalert2 -->
+<link rel="stylesheet" href="css/sweetalert2.min.css">
+
+
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
